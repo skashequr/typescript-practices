ee2b56202ba5e84ffd69202c05fdad3c3235d6f1
skashequr <learningwithextracare@gmail.com>
2024-09-22 14:51:10 +0600

    function i ts

---
+++ module-1/dist/function.js
@@ -0,0 +1,13 @@
+"use strict";
+// function fun1(num1,num2) {
+//     return num1*num2 
+// }
+// fun1(4,5);  this is normal javascript code . Function
+function additions(cout1, cout2) {
+    return cout1 + cout2;
+}
+// or second numbrer defign wich data type i returen heare
+// additions(4,"2")  //this cant posible
+additions(2, 3);
+//Arrow Function 
+const arrowFun = (add2, add3) => add2 * add3;
---
+++ module-1/dist/referenceType.js
@@ -0,0 +1,22 @@
+"use strict";
+// Objece
+const user = {
+    name: "sheikh",
+    age: 23,
+    school: "BMS",
+    isAdmin: false
+}; // this code is just javascript 
+// in typescript we can declear data types in this object
+const user2 = {
+    name: "sheikh",
+    age: 78,
+    contuctNumber: 2390230,
+    isAdmin: true,
+    company: "Doyel Digital", //cant change it
+    nid: 45345346
+};
+// if i have no user id then we use optional chining ? 
+// if i have a value theis value if fixed then we used 
+// company: "Doyel Digital"
+//this value we cant change 
+// if just red only cant change value we just add readonly
