9f0b742d65cabc309edbbf9fd45bcb4a603def20
skashequr <learningwithextracare@gmail.com>
2024-09-22 14:50:30 +0600

    function

---
+++ module-1/src/function.ts
@@ -0,0 +1,19 @@
+// function fun1(num1,num2) {
+//     return num1*num2 
+// }
+
+// fun1(4,5);  this is normal javascript code . Function
+
+function additions (cout1:number,cout2:number): number  { //heare we declear which data type i pass hea
+    return cout1+cout2
+}
+
+// or second numbrer defign wich data type i returen heare
+
+// additions(4,"2")  //this cant posible
+additions(2,3);
+
+
+//Arrow Function 
+
+const arrowFun = (add2:number,add3:number) : number => add2*add3
