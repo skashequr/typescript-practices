a34047fa266bad1d064b4600adb802a553e52e89
skashequr <learningwithextracare@gmail.com>
2024-09-22 14:26:49 +0600

    array and topple

---
+++ module-1/src/array.ts
@@ -0,0 +1,15 @@
+
+// const array2 : number = [3,34534] we cant write it in this type 
+const array01 : number[] = [2,3,4,55,4]; // its oky
+
+
+const arayString = ["sd","efwe","fg","sefwe","fwef"]   //this code is java script
+
+const arrayString2 : string[] = arayString;
+
+// tople
+
+const array03 : [string , boolean , number] = ["sheikh",true,5];
+
+const array5 : [number,string] = [5,"huh"];
+
