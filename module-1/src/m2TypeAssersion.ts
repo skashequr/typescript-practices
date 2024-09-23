eb68c643bbe6aa182db315d4e4ff5813d1f7fa28
skashequr <learningwithextracare@gmail.com>
2024-09-23 22:32:12 +0600

    typeAssersion

---
+++ module-1/src/m2TypeAssersion.ts
@@ -0,0 +1,16 @@
+{
+    let anything : any;
+    anything = "My name is asheq";
+
+    (anything as string).charAt(0);
+
+    function kgToGm(var1: number | string): string | number{
+        if (typeof var1 === "string") {
+            return parseFloat(var1);
+
+        } else {
+            
+        }
+        return 0 ;
+    }
+}
